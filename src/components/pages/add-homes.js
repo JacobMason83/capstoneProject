97aa434d1d3b826dbec94b9fa0430728d04595db
JacobMason83 <jmason062083@gmail.com>
2021-01-31 16:05:08 -0500

    got functionality and styling done for add homes page

--- src/components/pages/add-homes.js
+++ src/components/pages/add-homes.js
@@ -1,10 +1,99 @@
-import React from 'react'
 
-const AddHomes = (props) => {
+import React, { Component } from 'react'
+import axios from 'axios'
+// TODO change to react dropzone and style accordingly
+// TODO add a lease dropzone if possible
+
+export default class AddHomes extends Component {
+  constructor (props) {
+    super()
+    this.state = {
+      propertyName: '',
+      address: '',
+      value: '',
+      renter: '',
+      image: 'https://unsplash.com/s/photos/random-house'
+    }
+  }
+
+  handleChange = e => {
+    e.preventDefault()
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = () => {
+      const {propertyName, address,value,renter,image} = this.state
+    axios
+      .post('http://localhost:4000/homes',{
+          propertyName,
+          address,
+          value,
+          renter,
+          image
+      }, {withCredentials: true})
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+  render () {
     return (
-        <div className="add-homes-form">
-            <h1>Hi you added me woohoo</h1>
-        </div>
+      <div className='add-homes-form'>
+        <form className='landlord-form' onSubmit={this.handleSubmit}>
+          <h1>Add Your Properties Here:</h1>
+          <div className='input-wrapper'>
+            <label htmlFor='propertyName'>Property Name </label>
+            <input
+              type='text'
+              name='propertyName'
+              value={this.state.propertyName}
+              placeholder='Property Name'
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-wrapper'>
+            <label htmlFor='address'>Address </label>
+            <input
+              type='text'
+              name='address'
+              value={this.state.address}
+              placeholder='Address'
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-wrapper'>
+            <label htmlFor='value'>Property Value</label>
+            <input
+              type='text'
+              name='value'
+              value={this.state.value}
+              placeholder='Property Value'
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-wrapper'>
+            <label htmlFor='renter'>Renter Name </label>
+            <input
+              type='text'
+              name='renter'
+              value={this.state.renter}
+              placeholder='Renter Name'
+              onChange={this.handleChange}
+            />
+          </div>
+          <div className='input-wrapper'>
+            <label htmlFor='image'>Image of Property </label>
+            <input
+              type='text'
+              name='image'
+              value={this.state.image}
+              placeholder='Image of Property/leave blank'
+              onChange={this.handleChange}
+            />
+          </div>
+          <button className='btn' type="submit">Add-Home</button>
+        </form>
+      </div>
     )
+  }
 }
-export default AddHomes
--- src/components/pages/addRemoveTenant.js
+++
@@ -1,5 +0,0 @@
-//TODO 
-// create a form to accept data from the landlord
-// feilds needed 
-// property name , address, rent price, name of tenant, num of people, lease start date, lease end date, pic of lease, pic of property
-// setting of tenant account make username and password, and they have access to landlord, but not all landlord functionality 
