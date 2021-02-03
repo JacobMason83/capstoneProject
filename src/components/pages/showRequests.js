33a2bfce67b630f3d9306605b3ef8f575075a6a2
JacobMason83 <jmason062083@gmail.com>
2021-02-03 17:43:36 -0500

    added media queries form add homes page

--- src/components/pages/showRequests.js
+++ src/components/pages/showRequests.js
@@ -15,28 +15,19 @@ export default class RequestShowPage extends Component {
    getRequestItem = () => {
        axios
        .get('http://localhost:4000/all-requests', {withCredentials: true})
-       .then(res => this.setState({
+       .then(res => 
+            this.setState({
            needToBeFixed: res.data
        }))
        .catch(err => console.log('this.getRequestItem error', err))
    }
-//    showNeedtoBeFixed = () => {
-//        const { needToBeFixed } = this.state
-//        needToBeFixed.map(item => {
-//            return(
-//                <div key={item.id}>
-//                    <h1>{item.renter}</h1>
-//                    <h2>{item.address}</h2>
-//                    <p>{item.description}</p>
-//                </div>
-//            )
-//        })
-//    }
+   
    componentDidMount = () => {
    this.getRequestItem()
    }
 
    render() {
+      
      return (
          
         <ShowAllRequests needToBeFixed={this.state.needToBeFixed} />
--- src/components/user/maprequests.js
+++ src/components/user/maprequests.js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 
 export default function ShowAllRequests(props) {
     const items = props.needToBeFixed
 
    return(
-   <div className="request-wrapper"> 
+   <Fragment>
         {items.map(item => {
+           
             return(
-                <div key={item.id} className='request-card'>
-                    <div className="heading">
-                    <h1>{item.renter}</h1>
-                    <h2>{item.address}</h2>
+                <div key={item.id} className='request-card' >
+                        <h1>Maintanence Requests </h1>
+                    <div className="card-content">
+                    <h1>Renter <br /><hr></hr> Name: {item.renter}</h1>
                     
-                    </div>
-                    <div className="content">
-                    <p>{item.description}</p>
-                    </div>
+                    <h2>address <br /><hr></hr>{item.address}</h2>
+                   
+                    <p>Problem: <br />{item.description}</p>
+                    </div>                 
                 </div>
             )
         })}
-  </div>
+  </Fragment>
    )
 }
