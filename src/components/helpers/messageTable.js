564b4894e7357b236f65e96a3bd644ea72bed6ef
JacobMason83 <jmason062083@gmail.com>
2021-05-29 20:59:47 -0400

    addd styles to the messages component, and got it set up as a table

---
+++ src/components/helpers/messageTable.js
@@ -0,0 +1,43 @@
+import { DataGrid } from '@material-ui/data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'from', headerName: 'From', width: 130 },
+  { field: 'message', headerName: 'Message', width: 130 },
+  {
+    field: 'timestamp',
+    headerName: 'TimeStamp',
+    type: 'number',
+    width: 90,
+  },
+//   {
+//     field: 'fullName',
+//     headerName: 'Full name',
+//     description: 'This column has a value getter and is not sortable.',
+//     sortable: false,
+//     width: 160,
+//     valueGetter: (params) =>
+//       `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+//   },
+];
+
+
+//  [
+//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// ];
+
+export default function MessageTable(props) {
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={props.rows} columns={columns} pageSize={5} checkboxSelection />
+    </div>
+  );
+}
--- src/components/pages/messaging.js
+++ src/components/pages/messaging.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+// import MessageTable  from '../helpers/messageTable.js'
 //Todo get userinfo from state and use it to pass the id to the user message component
 const Messaging = props => {
   const [username, setUsername] = useState('')
-
   const [data, setData] = useState([])
 
   useEffect(() => {
@@ -17,28 +17,46 @@ const Messaging = props => {
       })
       .catch(err => console.error(err))
   }, [])
+  const rows = data.map(item => {
+    return (
+      { id: item.id,
+        message: item.msg,
+        from: item.from,
+        timestamp: item.timestamps.createdAt
+      }
+    )
+  })
+  console.log(rows)
   return (
-    <div className="messages-container">
-    <div className='message-container'>
-      {data.map(item => {
-        return (
-          <div className='get-messages' key={item.id}>
-            <div className='message-header'>
-              <h1>From: {item.from}</h1>
-            </div>
-            <div className='message-wrapper'>
-              <p>Message: {item.msg}</p>
-              <h6>sent at: {item.timestamps.createdAt}</h6>
-            </div>
-          </div>
-        )
-      })}
-    </div>
-    </div>
     
   
-   
+    <div className="messages-container">
+    <div className='message-container'>
+    <h1>Messages</h1>
+    <table className='messagesTable'>
+    <thead>
+
     
+  <tr>
+    <th>From</th>
+    <th>Message</th>
+    <th>sent@</th>
+  </tr>
+</thead>
+      {data.map(item => {
+        return (
+          <tbody key={item.id}>
+          <tr >
+            <td>{item.from}</td>
+            <td>{item.msg}</td>
+            <td>{item.timestamps.createdAt}</td>
+          </tr>
+          </tbody>
+        )
+      })}
+      </table>
+    </div>
+    </div>
   )
 }
 export default Messaging
